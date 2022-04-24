f465fde93df9a02377e11fe95cbb22787ff37f14
Rushith Nalin Vincent Karunaratne <rukarunaratne@gmail.com>
2022-04-24 21:46:24 +1000

    Ui changes

--- learn/src/components/Header.js
+++ learn/src/components/Header.js
@@ -1,11 +1,11 @@
 import Button  from "./Button"
 //import PropTypes from 'prop-types'
-const Header = ({onAdd,title}) => {
+const Header = ({onAdd,title,showAdd}) => {
   
     return (
       <header className = 'header'>
         <h1>{title}</h1> 
-        <Button  onClick = {onAdd} color = 'green' text = 'Add'  />
+        <Button  onClick = {onAdd} color = {showAdd ? 'red' : 'green'} text =  {showAdd ? 'Close' : 'Add'}  />
       </header>
     )
 }
